3ead17d25f3b5e4f08bf7396d586de5444760cdc
Cladnic <21344056+Cladnic@users.noreply.github.com>
2020-10-28 19:24:35 +0100

    Added global navigation bar
    Able to switch between scenen with the global naviagtion bar that I implemented

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,20 +4,45 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
+    private static BorderPane mainLayout;
+    private static Stage primaryStage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        this.primaryStage = primaryStage;
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("fxml/HomePage.fxml"));
+        mainLayout=loader.load();
         Parent root = FXMLLoader.load(getClass().getResource("fxml/HomePage.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Johans Airline booking");
         primaryStage.setMinHeight(400);
         primaryStage.setMinWidth(600);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        showMainView();
     }
 
+    public static void loadCenter(String resourceDirec) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource(resourceDirec));
+        BorderPane centerView = loader.load();
+        mainLayout.setCenter(centerView);
+    }
+
+    public static void showMainView() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("fxml/HomePage.fxml"));
+        mainLayout=loader.load();
+        Scene scene = new Scene(mainLayout);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
